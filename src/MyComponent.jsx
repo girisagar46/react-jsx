9834e3c5573c975ec962c61a3411219ae5242d25
Sagar Giri <sagar.giri@octoenergy.com>
2023-06-10 23:57:36 +0900

    Add my own component

---
+++ src/MyComponent.jsx
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl">Let's get started.</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
